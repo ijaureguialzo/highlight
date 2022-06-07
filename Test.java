63c5d9fea0ccfaae9ddf0efa166ca0cf0d45fc2a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-06-07 08:32:30 +0200

    Clase de prueba

---
+++ Test.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Test {
+    private static int x = 7;
+
+    /**
+     * Javadoc
+     *
+     * @param args Argumento
+     */
+    public static void main(String[] args) {
+        Random r = new Random();
+        int x = r.nextInt();
+        boolean a = false;
+        System.out.println("Pr\nueba");
+        System.out.println(4);
+        System.out.println(x + "a");
+        System.out.println(a);  // Comentario
+    }
+}
